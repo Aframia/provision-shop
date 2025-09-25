24efaa98382e7165490a98dc1b49a4225576e424
Aframia <168097776+Aframia@users.noreply.github.com>
2025-09-25 15:56:46 +0000

    addec basic html and styling

---
+++ script.js
@@ -0,0 +1,40 @@
+import products from "/data.js"
+const storeItems = document.getElementById("items")
+
+
+
+products.map( product => {
+    const {name} = product
+
+     storeItems.innerHTML +=`
+     <section id="${name}">
+        <h2>${name}</h2>
+        <div class="products" id="products">
+        ${displayProducts(product)}
+        </div>
+      </section>
+
+    `
+})
+
+function displayProducts(product){
+     return product.items.map( item => {
+    const {image,productDetails} = item
+    return `
+     <div class="product" id="product">
+        <img src="${image}" alt="${productDetails.productName}">
+        <div class="product-details">
+            <p class="product-name">${productDetails.productName}</p>
+            <div class="price-quantity">
+            <p class="quantity">Quantity: ${productDetails.quantity}</p>
+            <p class="price">GHC : ${productDetails.productPrice}</p>
+            </div>
+          </div>
+          <div class="product-buttons">
+            <button class="increment"> + </button>
+            <button class="decrement"> - </button>
+          </div>
+        </div>
+        <hr>`
+     }).join("")
+     }
