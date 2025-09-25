a133fd5f28833c361d7ad401e62a2f7235309940
Aframia <168097776+Aframia@users.noreply.github.com>
2025-09-25 21:04:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ products.map( product => {
     const {name} = product
 
      storeItems.innerHTML +=`
-     <section id="${name}">
+     <section id="${product.id}">
         <h2>${name}</h2>
         <div class="products" id="products">
         ${displayProducts(product)}
@@ -30,28 +30,45 @@ function displayProducts(product){
         <div class="product-details">
             <p class="product-name">${productDetails.productName}</p>
             <div class="price-quantity">
-            <p class="quantity">Quantity: ${productDetails.quantity}</p>
+            <p class="quantity" id="${productDetails.id}">Quantity: ${productDetails.quantity}</p>
             <p class="price">GHC : ${productDetails.productPrice}</p>
             </div>
           </div>
           <div class="product-buttons">
-            <button class="increment" data-action="increment"> + </button>
-            <button class="decrement"> - </button>
+            <button class="increment" data-add="${productDetails.id}"> + </button>
+            <button class="decrement" data-remove="${productDetails.id}"> - </button>
           </div>
         </div>
         <hr>`
      }).join("")
      }
 // increment and decrement functions
-const increment = document.querySelectorAll(".increment")
-const decrement = document.querySelectorAll(".decrement")
 
-function addQuantity(){
-    console.log("increment")
+document.addEventListener("click", function(e){
+    if(e.target.dataset.add){
+        findParent(e.target.dataset.add)
+        addQuantity(e.target.dataset.add)
+    
+    }
+   // if(e.target.dataset.remove){
+     //   removeQuantity(e.target.dataset.remove)
+    //}
+})
+function addQuantity(itemId){
+    const quantity= findProduct(itemId)
+    console.log(quantity)
+
 }
-addQuantity()
-function removeQuantity(){
-    console.log("decrement")
+function findParent(itemId){
+   products.filter(function(product){
+      return product.items.filter(function(item){
+        return item.items.filter(function(item){
+       if(item.productDetails.id === itemId){
+        return item.productDetails.quantity ++;
+       }
+    })
+}
+})
 }
 
-removeQuantity()
+
